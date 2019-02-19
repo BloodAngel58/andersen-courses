2e672fc0788f659d2b30ccdf9629ff4304510eee
BloodAngel58 <bloodange2013@yandex.ru>
2019-02-19 14:31:24 +0300

    Update script.js

--- Task 3/Task 3.3/script.js
+++ Task 3/Task 3.3/script.js
@@ -1,16 +1,8 @@
 var a = +prompt("Введите число ");
-var count = 0;
-var temp = a;
-var result = 0;
-while (temp != 0) {
-    temp = parseInt(temp / 10);
-    count++;
-}
-var i = count;
-for (var j = 1; j < count; j++) {
-
-    result += parseInt(a % Math.pow(10, j) / Math.pow(10, j - 1)) * Math.pow(10, i - 1);
-    i--;
-}
-result += parseInt(a / Math.pow(10, count - 1));
+var str = a.toString();
+var result = str
+  .split("")
+  .reverse()
+  .join("");
+console.log(result);
 alert(result);
